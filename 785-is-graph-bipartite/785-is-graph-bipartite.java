716a934426c130c9aba9692ab07db68cccbcc041
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-06-16 09:15:02 +0530

    Time: 1 ms (82.23%), Space: 54.2 MB (18.26%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,45 +1,41 @@
 class Solution {
     
-    public class Pair{
-        int src,group;
-        Pair(){}
-        Pair(int src,int group){
-            this.src = src;
-            this.group = group;
-        }
-    }
-    
-    public boolean isBipartite(int[][] graph) {
-        int[] vis = new int[graph.length];
-        Arrays.fill(vis,-1);
-        
-        for(int i=0;i<vis.length;i++){
-            if(vis[i]==-1){
-                Queue<Pair> qu = new LinkedList();
-                qu.add(new Pair(i,0));
-
-                while(qu.size()!=0){
-                    Pair rem = qu.remove();
-                    if(vis[rem.src] != -1){
-                        if(vis[rem.src] == rem.group){
-                            continue;
-                        }else{
-                            return false;
-                        }
-                    }
-
-                    vis[rem.src] = rem.group;
-
-                    for(int nbr : graph[rem.src]){
-                        qu.add(new Pair(nbr,(rem.group+1)%2));
-                    }
+    public boolean dfs(int[][] graph, int[] vis, int v , int color){
+        vis[v] = color;
+        int ncolor = color * -1;
+        for(int nbr : graph[v]){
+            if(vis[nbr] == 0 ){
+                boolean ans = dfs(graph , vis,nbr,ncolor); 
+                if(!ans){
+                    return false;
                 }
-
+            }else{
+                if(vis[nbr] != ncolor){
+                    return false;
+                }
             }
-            
         }
         
         return true;
+    }
+    
+    
+    
+    public boolean isBipartite(int[][] graph) {
         
+        int[] vis = new int[graph.length];
+        
+        boolean valid=true;
+        
+        for(int i=0; i<graph.length; i++){
+            if(vis[i]==0){
+                valid = dfs(graph,vis,i,1);
+                if(!valid){
+                    return false;
+                }    
+            }
+        }
+        
+        return valid;
     }
 }
