15b7e8dde38071d418e65f3c62c40720ef290e08
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-07-24 10:47:58 +0530

    Time: 3 ms (94.78%), Space: 54 MB (63.98%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -10,6 +10,10 @@ class Solution {
         
         
         long pairCount = 0;
+        
+        // looping till k/2 because numbers with remainders greater than k/2 has already been considered while froming pairs for numbers with remainder lesser than k/2
+        
+        // so to avoid repetation of same pair two times we looped till k/2 
         for(int rem=0;rem<=k/2;rem++){
             int n = freqMap[rem];
             
