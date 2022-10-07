c06e95a23e5c4dbe5a3c0c91483cf82cdee55cbb
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-10-07 08:19:59 +0530

    Time: 1 ms (98.35%), Space: 42.6 MB (90.57%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,30 +1,31 @@
 class Solution {
     List<List<Integer>> ans;
     public List<List<Integer>> permute(int[] nums) {
+        
+        boolean[] vis = new boolean[nums.length];
         ans = new ArrayList<>();
-        solver(new ArrayList<>(),nums);
+        solver(new ArrayList<>(),nums,vis,0);
         return ans;
     }
     
-    void solver(ArrayList<Integer> perm, int[] nums){
-        if(nums.length == 0){
+    void solver(ArrayList<Integer> perm, int[] nums , boolean[] vis,int count){
+        if(count == nums.length){
             List<Integer> base = new ArrayList<>(perm);
             ans.add(base);
             return;
         }
         
-        int[] n = new int[nums.length-1];
-        for(int ele : nums){
-            int idx = 0;
-            for(int i = 0;i<nums.length;i++){
-                if(nums[i] != ele){
-                    n[idx++]=nums[i]; 
-                }
+        for(int i=0;i<nums.length;i++){
+            if(!vis[i]){
+                vis[i] = true;
+                perm.add(nums[i]);
+                solver(perm , nums , vis, count+1);
+                perm.remove(perm.size()-1);
+                vis[i] = false;
             }
-            perm.add(ele);
-            solver(perm,n);
-            perm.remove(perm.size()-1);
         }
+        
+        
     }
     
 }
