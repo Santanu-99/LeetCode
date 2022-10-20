34092c1213a6a456fd73337a19c856e59f278598
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-10-20 19:00:39 +0530

    Time: 2 ms (88.17%), Space: 40.7 MB (72.91%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,48 +1,57 @@
+
 class Solution {
+    
     public int totalNQueens(int n) {
-        boolean mat[][] = new boolean[n][n];
-        int ans = getCount(mat, 0,0,n,n*n);
+        row = new boolean[n];
+        col = new boolean[n];
+        diag = new boolean[n+n-1];
+        antiDiag = new boolean[n+n-1];
+        int ans = nQueensSolver2(0,0,n);
         return ans;
     }
     
-    int[][] dir = {{0,-1},{-1,-1},{-1,0},{-1,1}};
-    public boolean isSafe(boolean[][] mat,int r, int c){
-        for(int d=0; d<dir.length; d++){
-            for(int j=1;j<mat.length;j++){
-                int nr = r + dir[d][0]*j;
-                int nc = c + dir[d][1]*j;
-                if(0<=nr && 0<=nc && nr<mat.length && nc < mat.length ){
-                    if(mat[nr][nc] == true){
-                        return false;
-                    }
-                }
-                else{
-                    break;
-                }
-            }
+    int dir[][] = {{-1,0},{-1,-1},{0,-1},{-1,1}};
+    boolean[] row , col , diag , antiDiag;
+    
+    boolean isSafe2(int r , int c , int n){
+        if(row[r]){
+            return false;
         }
+        if(col[c]){
+            return false;
+        }
+        if(diag[r-c+n-1]){
+            return false;
+        }
+        if(antiDiag[r+c]){
+            return false;
+        }
+        
         return true;
+    }
+    void toggle(int r,int c,int n){
+        row[r] = !row[r];
+        col[c] = !col[c];
+        diag[r-c+n-1] = !diag[r-c+n-1];
+        antiDiag[r+c] = !antiDiag[r+c];
     }
     
     
-    
-    public  int getCount(boolean[][] mat, int bsf, int qsf , int totQ , int totBox){
-        if(bsf == totBox || qsf==totQ ){
-            if(qsf == totQ){
+    int nQueensSolver2(int r , int qpsf , int n){
+        if(r == n || qpsf == n){
+            if(qpsf == n){
                 return 1;
+
             }
             return 0;
         }
-        int n = mat.length;
         int count = 0;
-        for(int b = bsf ; b<totBox ; b++){
-            int r = b / n;
-            int c = b % n;
-            if(isSafe(mat, r,c)){
-                mat[r][c] = true;
-                count += getCount(mat, b+1, qsf+1 ,totQ , totBox);
-                mat[r][c] = false;
-                
+        
+        for(int c = 0; c<n ; c++){
+            if(isSafe2(r, c, n)){
+                toggle(r ,c ,n);
+                count += nQueensSolver2(r+1 , qpsf+1 , n);
+                toggle(r, c, n);
             }
         }
         return count;
