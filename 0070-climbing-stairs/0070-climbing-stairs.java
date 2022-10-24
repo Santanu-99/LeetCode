a94636567bfe53b941fcb86b40dee7f071acf2c7
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-10-24 12:30:51 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (5.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        int ans = climbStairsMemo(0,n,dp);
+        // int ans = climbStairsMemo(0,n,dp);
+        int ans = climbStairsTabulation(0,n,dp);
         return ans;        
     }
     
@@ -20,6 +21,28 @@ class Solution {
                 count += climbStairsMemo(s+j , e , dp);
         }
         return dp[s]=count;
+    
+    }
+    
+    public int climbStairsTabulation(int S ,int E , int[] dp){
+        
+        for(int s=E;s >= S;s--){
+            if(s==E){
+                dp[s]=1;
+                continue;
+            } 
+
+            if(dp[s] != 0) return dp[s];
+
+            int count = 0;
+            for(int j=1;j<=2;j++){
+                if(s+j<=E)
+                    count += dp[s+j]; //climbStairsMemo(s+j , e , dp);
+            }
+            dp[s]=count;
+        }
+        
+        return dp[S];
     
     }
 
