b17eea66e826f6bbac6e701869e2400b5af9c462
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-12-03 00:05:20 +0530

    Time: 5 ms (80.59%), Space: 43.4 MB (81.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,16 +2,30 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         
-        boolean flag = false;
-        for(int i=0;i<nums.length && !flag ;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(target == (nums[i] + nums[j])){
-                    ans[0] = i;
-                    ans[1] = j;
-                    flag = true;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int ele : nums){
+            hm.put(ele,hm.getOrDefault(ele,0)+1);
+        }
+        
+        int fEle,sEle;
+        fEle=sEle=0;
+        
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])){
+                if(nums[i] != target-nums[i] || hm.get(nums[i]) > 1){
+                    fEle = nums[i];
+                    sEle = target - nums[i];
                     break;
                 }
             }
+        }
+        
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==fEle || nums[i] == sEle){
+                ans[count] = i;
+                count++;
+            } 
         }
         
         return ans;
