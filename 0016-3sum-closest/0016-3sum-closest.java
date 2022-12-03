02da0f05eb908197e791609cbec4a5d2408a4211
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-12-03 20:49:28 +0530

    Time: 38 ms (73.92%), Space: 44.4 MB (59.79%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -6,15 +6,23 @@ class Solution {
         int ans = 0;
         int diff = Integer.MAX_VALUE;
         for(int i=0;i<nums.length-2;i++){
-            for(int j=i+1;j<nums.length-1;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    sum = nums[i] + nums[j] + nums[k];
-                    if(Math.abs(target - sum) < diff){
-                        diff = Math.abs(target - sum);
-                        ans = sum;
-                    }
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                sum = nums[i] + nums[j] + nums[k];
+                if(Math.abs(target - sum) < diff){
+                    diff = Math.abs(target - sum);
+                    ans = sum;
+                }
+
+                if(sum>target){
+                    k--;
+                }
+                else{
+                    j++;
                 }
             }
+            
         }
         
         return ans;
