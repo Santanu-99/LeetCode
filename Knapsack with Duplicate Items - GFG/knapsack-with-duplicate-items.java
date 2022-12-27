8f265f0f33651736b3e48131cb70edecead510d7
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2022-12-27 11:18:08 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        int[][] dp = new int[N][W+1];
+         for(int[] ar : dp){
+             Arrays.fill(ar,-1);
+         }
+         int ans = getMaxVal(W,N-1,wt,val,dp);
+         return ans;
+    }
+    static int getMaxVal(int cap,int itm , int[] wt,int[] val,int[][] dp){
+        if(itm<0) return 0;
+        if(cap==0) return dp[itm][cap]=0;
+        
+        if(dp[itm][cap] != -1) return dp[itm][cap];
+        int inc = 0;
+        if(cap-wt[itm]>=0)
+            inc = getMaxVal(cap-wt[itm],itm,wt,val,dp) + val[itm];
+        int exc = getMaxVal(cap,itm-1,wt,val,dp);
+        
+        int myAns = Math.max(inc,exc);
+        dp[itm][cap]=myAns;
+        
+        return myAns;
+        
+    }
+}
