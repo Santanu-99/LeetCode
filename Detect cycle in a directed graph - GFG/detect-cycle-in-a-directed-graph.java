11e9345a2ab6c85ee5dbffa01bd77f5d339c2b10
Santanu-99 <59523439+Santanu-99@users.noreply.github.com>
2023-03-23 17:19:23 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -36,17 +36,59 @@ class Solution {
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
         // code here
         boolean[] vis = new boolean[V];
-        boolean[] stackpath = new boolean[V];
+        // boolean[] stackpath = new boolean[V];
         
-        for(int v=0;v<V;v++){
-            if(!vis[v]){
-                boolean cyclic = dfs(v , vis , stackpath , adj);
-                if(cyclic){
-                    return true;
-                }
-            }
+        // for(int v=0;v<V;v++){
+        //     if(!vis[v]){
+        //         boolean cyclic = dfs(v , vis , stackpath , adj);
+        //         if(cyclic){
+        //             return true;
+        //         }
+        //     }
+        // }
+        int[] inDegree = new int[V];
+        
+        
+        boolean cyclic = bfs_kahnAlgo(V, vis , inDegree , adj);
+        if(cyclic){
+            return true;
         }
         return false;
+    }
+    
+    boolean bfs_kahnAlgo(int V , boolean[] vis ,int[] inDegree, ArrayList<ArrayList<Integer>> adj ){
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int v=0;v<V;v++){
+            for(int nbr : adj.get(v)){
+                inDegree[nbr] += 1;
+            }
+        }
+        Queue<Integer> q= new ArrayDeque<>();
+        
+        for(int v=0;v<V;v++){
+            if(inDegree[v] == 0){
+                q.add(v);
+            }
+        }
+        
+        
+        while(q.size() > 0){
+            int remV = q.remove();
+            for(int nbr : adj.get(remV)){
+                inDegree[nbr] -= 1;
+                if(inDegree[nbr] == 0){
+                    q.add(nbr);
+                }
+            }
+            ans.add(remV);
+        }
+        
+        if(ans.size() == V){
+            return false;
+        }
+        return true;
+        
+        
     }
     
     boolean dfs(int src ,boolean[] vis ,boolean[] stackpath, ArrayList<ArrayList<Integer>> adj ){
